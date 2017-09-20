cb15ce66cde940e0e1e03651e3f64bc1208f1c18
truongpvuet <truongpvuet@gmail.com>
2017-09-21 02:33:30 +0700

    Done. Upload profile image without debugging errors on android.

--- android/app/src/main/java/com/blayncafe/MainApplication.java
+++ android/app/src/main/java/com/blayncafe/MainApplication.java
@@ -3,7 +3,6 @@ package com.blayncafe;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.airbnb.android.react.maps.MapsPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.farmisen.react_native_file_uploader.RCTFileUploaderPackage;
 import com.horcrux.svg.SvgPackage;
@@ -31,7 +30,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new MapsPackage(),
             new RNFetchBlobPackage(),
             new RCTFileUploaderPackage(),
             new SvgPackage(),
